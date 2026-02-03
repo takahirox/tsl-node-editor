7913a873442cf5f80324dd9994a754be76946cc8
Takahiro Aoyagi <hogehoge@gachapin.jp>
2026-02-03 17:08:26 +0900

    Fix viewer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tsl-node-editor/',
+  base: process.env.NODE_ENV === 'development' ? '/' : '/tsl-node-editor/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        viewer: resolve(__dirname, 'viewer.html'),
+      },
+    },
+  },
 })
